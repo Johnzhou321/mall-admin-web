9ff36dcabc85413b58a008064b8f896ee946b29a
John <you@example.com>
2019-08-26 18:04:51 +0800

    第一次提交

---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://47.106.142.52:8080"' //39.98.190.128
+}
---
+++ src/api/oss.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+export function policy() {
+  return request({
+    url:'/aliyun/oss/policy',
+    method:'get',
+    // url: '/upload/uploadFile',
+    // method: 'post',
+    // data: fd
+  })
+}
